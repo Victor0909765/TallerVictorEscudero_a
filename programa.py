470a83959c54f2dcf1cbfcfb631e0e238187e834
Victor0909765 <135990146+Victor0909765@users.noreply.github.com>
2023-06-15 14:51:15 +0000

    Taller funciones

---
+++ programa.py
@@ -0,0 +1,12 @@
+from matematicas import sumar, restar, multiplicar, dividir
+from algebra import distancia
+
+print(sumar(7,9))
+
+print(restar(4,2))
+
+print(multiplicar(7, 9))
+
+print(dividir(40, 8))
+
+print(distancia(9, 6, 3, 9))
